cdb4a93d736a962d3a7f6dadf94c6bb171d5e951
ombito <alvinombito09@gmail.com>
2023-07-11 14:48:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ let random = Math.floor(Math.random() + (1));
 const num3 = 24;
 const num4 = 5;
 
-const mod = (num3 % num4)
+const mod = (num3 % num4);
 
-let max = Math.max (20,10,15,12,17)
+let max = Math.max (20,10,15,12,17);
